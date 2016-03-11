0df6d2addf857d97ab403f0d4ed8e6799e7ec498
Nicklas Winger <nwi@gecko.no>
2016-03-11 08:16:46 +0100

    Initial commit. Need to convert to class syntax.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.join(__dirname),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    inline: true,
+    port: 8080,
+    historyApiFallback: true
+  },
+  module: {
+    preLoaders: [{
+      test: /\.js$/,
+      loader: 'eslint-loader',
+      include: path.join(__dirname, '/src'),
+      exclude: /node_modules/|/index\.js$/
+    }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
